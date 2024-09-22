9dd63c6b22459f85002adb8f8960b894fa69ba14
PRATAP-KUMAR <pratap@fastmail.fm>
2024-09-22 09:05:48 +0530

    final

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+const upload = multer();
 
 var app = express();
 
@@ -11,7 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+// solution
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
